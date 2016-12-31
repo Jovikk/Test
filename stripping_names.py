0e5419ddc82e4731193bde7d05c75cf4da52242b
Jovikk <jovikweb@gmail.com>
2016-12-31 09:21:38 -0700

    Add files via upload

---
+++ stripping_names.py
@@ -0,0 +1,8 @@
+name="\tjohn kourieh\t"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+print(name.upper().strip())
+print(name.lower().strip())
+print(name.title().strip())
