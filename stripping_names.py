25371d5ef823f7d9f02c056e1d4701c3ca3a3b79
Jovikk <jovikweb@gmail.com>
2016-12-31 09:24:54 -0700

    Add files via upload

---
+++ stripping_names.py
@@ -0,0 +1,9 @@
+#simple test that adds a tab before and after name, then uses stripping function to remove.
+name="\tJovik\t"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+print(name.upper().strip())
+print(name.lower().strip())
+print(name.title().strip())
